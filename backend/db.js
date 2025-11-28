478656660146dbba52116d4da0fe8a37a7a8b334
varis <varisanfalih@gmail.com>
2025-11-28 22:49:49 +0700

    fix again

--- backend/db.js
+++ backend/db.js
@@ -6,9 +6,12 @@ const { Pool } = pkg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: { rejectUnauthorized: false }
 });
+
+
+pool.connect()
+  .then(() => console.log("📌 PostgreSQL Connected Successfully"))
+  .catch(err => console.error("❌ Database Connection Failed:", err.message));
 
 export default pool;
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import budgetRoutes from './routes/budgetRoutes.js';
 import transactionRoutes from './routes/transactionRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
-
+import "./db.js";  
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
